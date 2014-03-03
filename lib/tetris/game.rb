441e18aca5b47b2dc5bcda83858729178f51197a
Dan Tripp <rubyfolksinger@gmail.com>
2014-03-03 15:59:13 -0800

    Added current_piece and next_piece to Game

--- lib/tetris/game.rb
+++ lib/tetris/game.rb
@@ -1,9 +1,11 @@
 module Tetris
   class Game
-    attr_accessor :gameboard
+    attr_accessor :gameboard, :current_piece, :next_piece
 
     def initialize( height=20, width=10 )
-      @gameboard = Tetris::Gameboard.new( height, width, nil )
+      @gameboard     = Tetris::Gameboard.new( height, width, nil )
+      @current_piece = Tetris::Tetrimino.sample
+      @next_piece    = Tetris::Tetrimino.sample
     end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,6 +6,14 @@ describe 'Tetris::Game' do
   end
 
   it 'should have a gameboard' do
-    Tetris::Game.new(10, 10).gameboard.class.should == Tetris::Gameboard
+    Tetris::Game.new( 10,10 ).gameboard.class.should == Tetris::Gameboard
+  end
+
+  it 'should have a current_piece' do
+    Tetris::Game.new( 5,5 ).current_piece.class.should == Tetris::Tetrimino
+  end
+
+  it 'should have a next_piece' do
+    Tetris::Game.new( 10,10 ).current_piece.class.should == Tetris::Tetrimino
   end
 end
