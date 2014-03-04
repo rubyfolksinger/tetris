255429e52e08b7c4e707a8a77e44c37de749f5c8
Dan Tripp <rubyfolksinger@gmail.com>
2014-03-04 11:17:25 -0800

    Robot#decide added

--- lib/tetris/robot.rb
+++ lib/tetris/robot.rb
@@ -1,4 +1,7 @@
 module Tetris
   class Robot
+    def self.decide( gameboard, current_piece )
+      0
+    end
   end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -4,4 +4,21 @@ describe 'Tetris::Robot' do
   it 'should exist' do
     Tetris::Robot.class.should == Class
   end
+
+  describe '#decide' do
+    before do
+      @gameboard = Tetris::Gameboard.new( 20, 10 )
+      @attributes = {
+        :height    => 2,
+        :width     => 3,
+        :shape     => [0,1,0,
+                       1,1,1]
+      }
+      @piece = Tetris::Tetrimino.new( @attributes )
+    end
+
+    it 'should choose the leftmost column if the board is empty' do
+      Tetris::Robot.decide( @gameboard, @piece ).should == 0
+    end
+  end
 end
