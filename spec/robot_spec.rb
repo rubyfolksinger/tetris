973a7a3fe83ae937826a7d6bfefb80a4a86b643e
Dan Tripp <rubyfolksinger@gmail.com>
2014-03-04 16:55:35 -0800

    Stupid-simple robot logic

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -11,14 +11,31 @@ describe 'Tetris::Robot' do
       @attributes = {
         :height    => 2,
         :width     => 3,
-        :shape     => [0,1,0,
-                       1,1,1]
+        :shape     => [ 1,1,
+                        1,1 ]
       }
       @piece = Tetris::Tetrimino.new( @attributes )
     end
 
     it 'should choose the leftmost column if the board is empty' do
       Tetris::Robot.decide( @gameboard, @piece ).should == 0
+    end
+
+    it 'should choose the third column if a 2x2 block is in the bottom left' do
+      @gameboard.drop!( @piece, 0 )
+      Tetris::Robot.decide( @gameboard, @piece ).should == 3
+    end
+
+    it 'should choose the fifth column if two 2x2 blocks are in the bottom left' do
+      @gameboard.drop!( @piece, 0 )
+      @gameboard.drop!( @piece, 2 )
+      Tetris::Robot.decide( @gameboard, @piece ).should == 5
+    end
+
+    it 'should choose the sixth column if two 2x2 blocks are in the bottom left' do
+      @gameboard.drop!( @piece, 0 )
+      @gameboard.drop!( @piece, 3 )
+      Tetris::Robot.decide( @gameboard, @piece ).should == 6
     end
   end
 end
